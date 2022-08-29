758147b4eaf95f6faf7d193081fa9c024a68f895
Jon Eastin <picardncce@gmail.com>
2022-08-29 13:37:56 -0700

    finished solving problems with trees walkthrough

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -64,6 +64,56 @@ class BinarySearchTree {
 		}
 	}
 
+	getHeight(currentHeight = 0) {
+		// BASE CASE:
+		// If the current node doesn't have a left or right child,
+		// then the base case is reached, and the function can return the height/
+		if (!this.left && !this.right) return currentHeight
+
+		// RECURSIVE CASE:
+		// Otherwise, compute the new height.
+		const newHeight = currentHeight + 1
+
+		// If there's no left child, recurse down the right subtree only,
+		// passing down the height of the current node
+		if (!this.left) return this.right.getHeight(newHeight)
+
+		// If there's no right child, recurse down both subtrees,
+		// passing down the height of the current node.
+		const leftHeight = this.left.getHeight(newHeight)
+		const rightHeight = this.right.getHeight(newHeight)
+
+		// Return the greater of the left or the right subtree heights
+		return Math.max(leftHeight, rightHeight)
+	}
+
+	isBST() {
+		// Use the existing `dfsInOrder()` method to traverse the tree.
+		const values = this.dfsInOrder()
+
+		// Check if the array returned by the in-order DFS is a sorted array
+		for (let i = 0; i < values.length; i++) {
+			// Compare the current and previous values.
+			if (values[i] < values[i - 1]) {
+				return false
+			}
+		}
+		return true
+	}
+
+	findKthLargestValue(k) {
+		// Use the existing `dfsInOrder()` method to traverse the tree.
+		const values = this.dfsInOrder()
+		const kthIndex = values.length - k
+
+		// Ensure that the index is within the bounds of the array.
+		if (kthIndex >= 0) {
+			return values[kthIndex]
+		} else {
+			console.log('k value exceeds the size of the BST.')
+		}
+	}
+
 	dfsInOrder(values = []) {
 		// First, process the left node recursively
 		if (this.left) {
